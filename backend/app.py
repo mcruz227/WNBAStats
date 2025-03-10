8b12090589618426b89a5f5d62a9d6f44f71e596
monica <121968692+mcruz227@users.noreply.github.com>
2025-03-10 16:49:17 -0400

    Updated Backend Flask

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 @app.route("/")
 def home():
     return "Wlcome to the wnba analystics api"
+
+df = pd.read_csv("backend/data/wnba_cleaned.csv")
+
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    """retunr smal sample"""
+    return jsonify(df.head(10).to_dict(orient='records'))
 
 if __name__ == "__main__":
     app.run(debug=True)
