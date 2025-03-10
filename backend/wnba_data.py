b1e2a6ffed700c753c9c31ab09a70f8a1671b9cc
monica <121968692+mcruz227@users.noreply.github.com>
2025-03-10 13:47:43 -0400

    Updated Clean CSV File

---
+++ backend/wnba_data.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os 
+
+def load_data():
+    file_path = "backend/data/wnba_box_scores_merge.csv"
+    cleaned_path = "backend/data/wnba_cleaned.csv"
+
+
+    if not os.path.exists(file_path):
+        print(f"ERROR: {file_path} not found!")
+        return
+
+    print(f"found file: {file_path}")
+
+
+    df = pd.read_csv(file_path)
+    print("file load success")
+
+
+    df.dropna(inplace= True)
+
+
+
+    columns_to_drop = ["fast_break_points", "points_in_paint", "turnover_points", "largest_lead",
+                       "team_color", "team_alternate_color", "team_logo", "opponent_team_color",
+       "opponent_team_alternate_color", "opponent_team_logo",
+       "opponent_team_score"]
+    print("dropping unnecessary columns..")
+    
+    df.drop(columns=columns_to_drop, inplace= True, errors="ignore")
+
+    # cleaned_file_path = "data/wnba_cleaned.csv"
+    df.to_csv(cleaned_path, index=False)
+
+    print(f"cleaned data saved to {cleaned_path}")
+    
+
+    return df
+
+
+
+if __name__ == "__main__":
+    load_data()
+
