d8537d45f32cdf85c057b3913e9746a7821604c9
monica <121968692+mcruz227@users.noreply.github.com>
2025-04-21 22:15:46 -0400

    relative paths

--- backend/app.py
+++ backend/app.py
@@ -23,28 +23,10 @@ def get_data():
 
     
   
-    df = pd.read_csv('/Users/monic/Documents/GitHub/WNBAStats/backend/data/team_summary.csv')
+    df = pd.read_csv(os.path.join("data", "team_summary.csv"))
 
-    # df['team-name'] = df['team_name'].astype(str).str.strip().str.lower()
-    # # df['team_winner'] = df['team_winner'].astype(str).str.strip().str.lower()
-    # df['season'] = df['season'].astype(str)
 
-    # # df['Win'] = (df['team_name'] == df['team_winner']).astype(int)
-    # # df['Loss'] = (df['team_name'] == df['team_winner']).astype(int)
-
-    # grouped = df.groupby(["team_name", "season"]).agg({
-    #     "Win": "sum",
-    #     "Loss": "sum"
-    # }).reset_index()
-
-    # grouped["win_pct"] = grouped["Win"] / (grouped["Win"] + grouped["Loss"])
-
-    # grouped["status"] = grouped["win_pct"].apply(lambda x: "Prime" if x >= 0.5 else "Rebuilding")
-   
-    # grouped.columns = ["team_name", "season", "wins", "losses", "win_pct", "status"]
-   
-   
     return jsonify(df.to_dict(orient='records'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
